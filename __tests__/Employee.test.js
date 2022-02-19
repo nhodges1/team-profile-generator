274a3ba05b91ade8771d2630c599711fc56a3a07
nhodges <hodges27@outlook.com>
2022-02-19 01:59:08 -0500

    test78

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -28,3 +28,21 @@ test("Can get name via getName()", () => {
     const e = new Employee(testValue);
     expect(e.getName()).toBe(testValue);
 });
+
+test("Can get id via getId()", () => {
+    const testValue = 100;
+    const e = new Employee("Foo", testValue);
+    expect(e.getId()).toBe(testValue);
+});
+
+test("Can get email via getEmail()", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
+
+test("getRole() should return \"Employee\"", () => {
+    const testValue = "Employee";
+    const e = new Employee("Alice", 1, "test@test.com");
+    expect(e.getRole()).toBe(testValue);
+});
