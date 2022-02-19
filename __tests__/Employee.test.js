3aea0bd5e778e38b1fcf5c1015e9fa21fa8958c6
nhodges <hodges27@outlook.com>
2022-02-19 01:44:05 -0500

    test2and3

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,3 +5,14 @@ test("Can instantiate Employee instance", () => {
     expect(typeof(e)).toBe("object");
 });
 
+test("Can set name via constructor arguments", () => {
+    const name = "Alice";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
+
+test("Can set id via constructor argument", () => {
+    const testValue = 100;
+    const e = new Employee("Foo", testValue);
+    expect(e.id).toBe(testValue);
+});
